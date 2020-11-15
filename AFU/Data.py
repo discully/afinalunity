18efb8ba032ea3b7fa4bc1ea8bca5f29624b37b2
Daniel I Scully <discully@users.noreply.github.com>
2020-11-15 16:02:22 +0000

    add handler for trigger files

---
+++ AFU/Data.py
@@ -0,0 +1,46 @@
+from AFU.File import File
+from AFU.Block import _readObjectId
+from enum import Enum
+
+
+class TriggerType (Enum):
+	NORMAL = 0x0
+	TIMER = 0x1
+	PROXIMITY = 0x2
+	UNUSED = 0x3
+
+
+def triggers(file_path):
+	f = File(file_path)
+	data = []
+	while not f.eof():
+		trigger = {
+			"id": f.readUInt32()
+		}
+		
+		assert(f.readUInt16() == 0xffff)
+		trigger["unknown_a"] = f.readUInt8()
+		assert (f.readUInt8() == 0xff);
+		trigger["type"] = TriggerType(f.readUInt32())
+		trigger["target"] = _readObjectId(f)
+		trigger["enabled"] = bool( f.readUInt8() )
+		trigger["unknown_b"] = f.readUInt8()
+		assert (f.readUInt16() == 0xffff);
+		trigger["timer_start"] = f.readUInt32()
+	
+		if trigger["type"] == TriggerType.TIMER:
+			assert( f.readUInt32() == 0)
+			assert( f.readUInt32() == 0)
+		elif trigger["type"] == TriggerType.PROXIMITY:
+			trigger["distance"] = f.readUInt16()
+			assert(f.readUInt16() == 0xffff)
+			trigger["from"] = _readObjectId(f)
+			trigger["to"] = _readObjectId(f)
+			trigger["reversed"] = bool(f.readUInt8())
+			trigger["instant"] = bool(f.readUInt8())
+			assert(f.readUInt16() == 0xffff)
+		elif trigger["type"] == TriggerType.UNUSED:
+			assert(f.readUInt32() == 0)
+		
+		data.append(trigger)
+	return data
