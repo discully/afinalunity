f1bf0e1bb54f061c589bf8fff1cd59f253ef4088
discully <discully@users.noreply.github.com>
2025-12-17 23:05:55 +0000

    Handle SILE blocks

--- AFU/Sprite.py
+++ AFU/Sprite.py
@@ -180,6 +180,9 @@ def sprite(sprite_path, background_path, palette_path=None):
 		
 		elif block["name"] == "OBJS":
 			block["obj_state"] = f.readUInt32()
+		
+		elif block["name"] == "SILE":
+			pass
 
 		else:
 			raise ValueError("Unknown block {} at {:#x}".format(block["name"], block["offset"]))
