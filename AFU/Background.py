1a7fc1506b03d5885bd2b05325822435023c11d3
discully <discully@users.noreply.github.com>
2025-12-28 16:33:14 +0000

    Fix bug in .pic backgrounds

--- AFU/Background.py
+++ AFU/Background.py
@@ -6,12 +6,22 @@ HEIGHT = 480
 
 
 def background(input_path, palette_path=None):
-	
-	palette_path = Palette.getGlobalPalettePath(input_path, palette_path)
-	global_palette = Palette.singlePalette(palette_path)
+
+	# TODO: Will not work for sb002004.scr, sb002007.scr, sb002009.scr, sb002016.scr, sb002018.scr, sb005012.scr, sb006015.scr,sb006016.scr, sb007004.scr
 
 	f = File.File(input_path)
+
+	n_palettes = (len(f) - (WIDTH * HEIGHT)) // (Palette._LENGTH_SINGLE * 3)
+
+	if n_palettes < 1 or n_palettes > 2:
+		raise ValueError("Background file contains an unexpected number of palettes: {}".format(n_palettes))
+
 	local_palette = Palette.readSinglePalette(f)
+	if n_palettes == 1:
+		palette_path = Palette.getGlobalPalettePath(input_path, palette_path)
+		global_palette = Palette.singlePalette(palette_path)
+	else:
+		global_palette = Palette.readSinglePalette(f)
 	palette = Palette.combinePalettes(local_palette, global_palette)
 	
 	image = Image.Image(WIDTH, HEIGHT)
@@ -21,6 +31,8 @@ def background(input_path, palette_path=None):
 	for pixel in range(n_pixels):
 		colour_index = f.readUInt8()
 		image.set(palette[colour_index], pixel)
+
+	assert(f.eof())
 	
 	return {
 		"name": input_path.name,
