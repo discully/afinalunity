28ce042a7d1c98d3c188e26a3710f500594b1cf1
discully <discully@users.noreply.github.com>
2026-01-17 12:29:48 +0000

    Finish enum based file identification, and begin tidying up imports

--- AFU/__init__.py
+++ AFU/__init__.py
@@ -1,4 +1,30 @@
+from enum import Enum
+
+
+from . import Astro
+from . import Background
+from . import Block
+from . import Computer
+from . import Cursor
+from . import Data
+from . import File
+from . import Font
+from . import Image
+from . import Graphics
+from . import Menu
+from . import Map
+from . import Palette
+from . import SaveGame
+from . import Sprite
+from . import Tactic
+from . import Terminal
+from . import Utils
+from . import Video
+from . import World
+
+
 __all__ = [
+	"FileType", "handler", "identify"
 	"Astro",
 	"Background",
 	"Block",
@@ -20,4 +46,136 @@ __all__ = [
 	"Video",
 	"World",
 	]
-from . import *
+
+
+class FileType (Enum):
+	ADVICE = 0
+	ALERT = 1
+	ASTRO_STATE = 2
+	AUDIO = 3
+	COMPUTER_STATE = 4
+	CONVERSATION = 5
+	CREDITS = 6
+	CURSOR = 7
+	DATABASE_ASTRO = 8
+	DATABASE_ASTROMAP = 9
+	DATABASE_COMPUTER = 10
+	FONT = 11
+	G3D_MATERIAL = 12
+	G3D_OBJECT = 13
+	ICON_MAP = 14
+	IMG_BACKGROUND = 15
+	IMG_GIF = 16
+	IMG_LBM = 17
+	IMG_MENU = 18
+	LIST = 19
+	MOVIE_MAP = 20
+	OBJECT = 21
+	PALETTE = 22
+	PHASER = 23
+	PHASER_MAP = 24
+	POLYGONS = 25
+	SAVEGAME = 26
+	SECTOR_NAMES = 27
+	SPRITE = 28
+	TACTIC = 29
+	TERMINAL = 30
+	TEXT = 31
+	TRIGGERS = 32
+	UNKNOWN = 33
+	VIDEO = 34
+	WORLD = 35
+	WORLD_LIST = 36
+	WORLD_OBJECTS = 37
+	WORLD_START = 38
+
+
+_FILE_HANDLERS = {
+	FileType.DATABASE_ASTRO:      Astro.astroDb,
+	FileType.DATABASE_ASTROMAP:   Astro.astromapDb,
+	FileType.ASTRO_STATE:         Astro.astStatDat,
+	FileType.DATABASE_COMPUTER:   Computer.computerDb,
+	FileType.COMPUTER_STATE:      Computer.compstat,
+	FileType.WORLD:               World.worldSlScr,
+	FileType.POLYGONS:            World.worldStScr,
+	FileType.SPRITE:              Sprite.sprite,
+	FileType.SECTOR_NAMES:        Astro.sectorAst,
+	FileType.CONVERSATION:        Block.bst,
+	FileType.OBJECT:              Block.bst,
+	FileType.PHASER:              Block.bst,
+	FileType.LIST:                Sprite.lst,
+	FileType.ADVICE:              World.adviceDat,
+	FileType.TERMINAL:            Terminal.terminal,
+	FileType.WORLD_START:         World.worldStrt,
+	FileType.WORLD_LIST:          World.worldList,
+	FileType.WORLD_OBJECTS:       World.worldObj,
+	FileType.TRIGGERS:            Data.triggers,
+	FileType.ICON_MAP:            Map.icon,
+	FileType.MOVIE_MAP:           Map.movie,
+	FileType.PHASER_MAP:          Map.phaser,
+	FileType.SAVEGAME:            SaveGame.savegame,
+	FileType.TACTIC:              Tactic.bin,
+	FileType.ALERT:               Data.alert,
+	FileType.FONT:                Font.font,
+	FileType.IMG_MENU:            Menu.mrg,
+	FileType.PALETTE:             Palette.pal,
+	FileType.TEXT:                Data.txt,
+	FileType.CREDITS:             Data.credits,
+	FileType.G3D_MATERIAL:        Graphics.material,
+	FileType.IMG_GIF:             Graphics.img,
+	FileType.IMG_LBM:             Graphics.lbm,
+	FileType.G3D_OBJECT:          Graphics.model,
+	FileType.VIDEO:               Video.fvf,
+}
+
+
+def handler(file_type):
+	if not file_type in _FILE_HANDLERS:
+		raise ValueError(f"{file_type} is currently unsupported")
+	return _FILE_HANDLERS[file_type]
+
+
+def identify(file_path):
+	type_str = Utils.identify(file_path)
+	type_map = {
+		"advice": FileType.ADVICE,
+		"alert": FileType.ALERT,
+		"astro_state": FileType.ASTRO_STATE,
+		"audio": FileType.AUDIO,
+		"computer_state": FileType.COMPUTER_STATE,
+		"conversation": FileType.CONVERSATION,
+		"credits": FileType.CREDITS,
+		"cursor": FileType.CURSOR,
+		"database_astro": FileType.DATABASE_ASTRO,
+		"database_astromap": FileType.DATABASE_ASTROMAP,
+		"database_computer": FileType.DATABASE_COMPUTER,
+		"font": FileType.FONT,
+		"material": FileType.G3D_MATERIAL,
+		"3dobject": FileType.G3D_OBJECT,
+		"icon_map": FileType.ICON_MAP,
+		"background": FileType.IMG_BACKGROUND,
+		"image_gif": FileType.IMG_GIF,
+		"image_lbm": FileType.IMG_LBM,
+		"menu": FileType.IMG_MENU,
+		"list": FileType.LIST,
+		"movie_map": FileType.MOVIE_MAP,
+		"object": FileType.OBJECT,
+		"palette": FileType.PALETTE,
+		"phaser": FileType.PHASER,
+		"phaser_map": FileType.PHASER_MAP,
+		"polygons": FileType.POLYGONS,
+		"savegame": FileType.SAVEGAME,
+		"sector_names": FileType.SECTOR_NAMES,
+		"sprite": FileType.SPRITE,
+		"tactic": FileType.TACTIC,
+		"terminal": FileType.TERMINAL,
+		"text": FileType.TEXT,
+		"triggers": FileType.TRIGGERS,
+		"video": FileType.VIDEO,
+		"world": FileType.WORLD,
+		"world_list": FileType.WORLD_LIST,
+		"world_objects": FileType.WORLD_OBJECTS,
+		"start": FileType.WORLD_START,
+		"unknown": FileType.UNKNOWN,
+	}
+	return type_map.get(type_str, FileType.UNKNOWN)
--- afu_to_json.py
+++ afu_to_json.py
@@ -57,91 +57,29 @@ def main():
 	parser.add_argument("-o", "--output_dir", type=Path, help="Output directory to place json in", default=".")
 	args = parser.parse_args()
 
-	file_type = AFU.Utils.identify( args.file )
+	file_type = AFU.identify( args.file )
+	file_handler = AFU.handler(file_type)
 
-	if file_type == "database":
-		if args.file.stem == "computer":
-			data = AFU.Computer.computerDb(args.file)
-			if not args.image_data: # Strip image data out
+	if file_type == AFU.FileType.SPRITE:
+		data = file_handler(args.file, args.file.with_name("standard.pal"), args.file.with_name("standard.pal"))
+	else:
+		data = file_handler(args.file)
+	
+	if file_type == AFU.FileType.DATABASE_COMPUTER:
+		if not args.image_data:
 				for value in data.values():
 					if "image" in value:
 						value["image"].pop("data")
-		elif args.file.stem == "astro":
-			data = AFU.Astro.astroDb(args.file)
-		elif args.file.stem == "astromap":
-			data = AFU.Astro.astromapDb(args.file)
-		else:
-			print("Unsupported database file: {}".format(args.file.name))
-			return
-	elif file_type == "astro_state":
-		data = AFU.Astro.astStatDat(args.file)
-	elif file_type == "computer_state":
-		data = AFU.Computer.compstat(args.file)
-	elif file_type == "world":
-		data = AFU.World.worldSlScr(args.file)
-	elif file_type == "polygons":
-		data = AFU.World.worldStScr(args.file)
-	elif file_type == "sprite":
-		data = AFU.Sprite.sprite(args.file, args.file.with_name("standard.pal"), args.file.with_name("standard.pal"))
+	elif file_type == AFU.FileType.SPRITE:
 		_stripSpriteAudio(args.file, data)
-	elif file_type == "sector_names":
-		data = AFU.Astro.sectorAst(args.file)
-	elif file_type in ["conversation", "object", "phaser"]:
-		data = AFU.Block.bst(args.file)
+	elif file_type in [AFU.FileType.CONVERSATION, AFU.FileType.OBJECT, AFU.FileType.PHASER]:
 		_stripDebugElements(data)
 		_stripUnknownElements(data)
-	elif file_type == "list":
-		data = AFU.Sprite.lst(args.file)
-	elif file_type == "advice":
-		data = AFU.World.adviceDat(args.file)
-	elif file_type == "terminal":
-		data = AFU.Terminal.terminal(args.file)
-	elif file_type == "start":
-		data = AFU.World.worldStrt(args.file)
-	elif file_type == "world_list":
-		data = AFU.World.worldList(args.file)
-	elif file_type == "world_objects":
-		data = AFU.World.worldObj(args.file)
-	elif file_type == "triggers":
-		data = AFU.Data.triggers(args.file)
-	elif file_type == "icon_map":
-		data = AFU.Map.icon(args.file)
-	elif file_type == "movie_map":
-		data = AFU.Map.movie(args.file)
-	elif file_type == "phaser_map":
-		data = AFU.Map.phaser(args.file)
-	elif file_type == "savegame":
-		data = AFU.SaveGame.savegame(args.file)
-	elif file_type == "tactic":
-		data = AFU.Tactic.bin(args.file)
+	elif file_type == AFU.FileType.TACTIC:
 		_stripDebugElements(data)
 		_stripTacticMetadata(data)
-	elif file_type == "alert":
-		data = AFU.Data.alert(args.file)
-	elif file_type == "font":
-		data = AFU.Font.font(args.file)
-	elif file_type == "menu":
-		data = AFU.Menu.mrg(args.file)
-	elif file_type == "palette":
-		data = AFU.Palette.pal(args.file)
-	elif file_type == "text":
-		data = AFU.Data.txt(args.file)
-	elif file_type == "credits":
-		data = AFU.Data.credits(args.file)
-	elif file_type == "material":
-		data = AFU.Graphics.material(args.file)
-	elif file_type == "image_gif":
-		data = AFU.Graphics.img(args.file)
-	elif file_type == "image_lbm":
-		data = AFU.Graphics.lbm(args.file)
-	elif file_type == "3dobject":
-		data = AFU.Graphics.model(args.file)
-	elif file_type == "video":
-		data = AFU.Video.fvf(args.file)
+	elif file_type == AFU.FileType.VIDEO:
 		_stripVideoData(data)
-	else:
-		print("Unsupported file type: {}".format(file_type))
-		return
 
 	output_path = "{}.json".format(args.output_dir.joinpath(args.file.name))
 	json.dump(data, open(output_path, "w"), indent="\t", cls=AFU.Utils.Encoder)
